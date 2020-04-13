9a436d9859ad66edb7a2569254e59208d155680f
Luca Andrei <andreilucalink@gmail.com>
2020-04-13 17:42:31 +0300

    modal fix loader

--- src/layout/ConfirmAgeModal.js
+++ src/layout/ConfirmAgeModal.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+import Img from 'react-image';
 
 function ConfirmAgeModal(props) {
 
 
     const acceptAgeReq = () => {
-        debugger;
         window.removeEventListener('scroll', props.scrollListener);
         sessionStorage.setItem('clientAge', 'valid');
         document.getElementById("home-page").classList.remove('hide-home');
@@ -20,7 +21,12 @@ function ConfirmAgeModal(props) {
     return (
         <div className="confirmModal hide-confModal" id="confirmModal">
 			<h2>Trebuie să aveţi împliniţi <b>18 ani</b> pentru a accesa acest site</h2>
-			<img src={require('../img/lf.png')} alt="logo" className="modalLogo"/>
+			{/* <img src={require('../img/lf.png')} alt="logo" className="modalLogo"/> */}
+            <Img
+					className="modalLogo"
+					src={require('../img/lf.png')}
+					loader={<Spinner animation="grow" className="modalLogoLoader" />}
+			/>
 			<div className="confirmButtons">
 				<button className="ageConfirm" onClick={()=> acceptAgeReq()}>Accept</button>
 				<button className="ageDeny" onClick={()=> denyAgeReq()}>Refuz</button>
