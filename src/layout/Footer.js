32cd8feb26ac9e24f40f75b17a2db44dbf2483e7
Luca Andrei <andreilucalink@gmail.com>
2020-05-09 16:54:12 +0300

    test fix social 2

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -7,22 +7,20 @@ import {isAndroid, isIOS, isMobile} from 'react-device-detect';
 
 
 function Footer() {
-  const [facebookLink, setFacebookLink] = useState("https://www.facebook.com/cramaferdi");
-  const [instagramLink, setInstagramLink] = useState("https://www.instagram.com/ferdi_winery/")
 
+  let fbLink = "https://www.facebook.com/cramaferdi";
+  let igLink = "https://www.instagram.com/ferdi_winery/";
   if(isMobile){
     if (isAndroid) {
-      setFacebookLink("fb://page/492360557613539");
-      setInstagramLink("instagram://user?username=ferdi_winery");
+      fbLink = "fb://page/492360557613539";
+      igLink = "instagram://user?username=ferdi_winery";
     }
     if (isIOS) {
-      setFacebookLink("fb://profile/492360557613539");
-      setInstagramLink("instagram://user?username=ferdi_winery");
+      fbLink = "fb://profile/492360557613539";
+      igLink = "instagram://user?username=ferdi_winery";
     }
   }
 
-
-  
   return (
     <footer className="page-footer footer" id="ferdi-footer">
       <div className="footer-copyright text-center">
@@ -34,10 +32,10 @@ function Footer() {
         <Nav.Link className="footer-link" as={Link} eventKey="1" to="/contact">Contact</Nav.Link>
       </div>
       <div className="social-media-btns">
-        <a href={facebookLink} target="_blank" rel="noopener noreferrer" className="fb-logo">
+        <a href="www.test.com" target="_blank" rel="noopener noreferrer" className="fb-logo">
           <img src={require('../img/facebook.png')}  alt="FB"/>
         </a>
-        <a href={instagramLink} target="_blank"  rel="noopener noreferrer" className="ig-logo">
+        <a href="www.test.com" target="_blank"  rel="noopener noreferrer" className="ig-logo">
         <img src={require('../img/instagram.png')}  alt="FB"/>
         </a>
       </div>
