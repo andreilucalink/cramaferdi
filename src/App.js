3a63e9c80e1b025caae010d583385f19dc6fcb40
Luca Andrei <andreilucalink@gmail.com>
2021-09-15 16:07:10 +0300

    removed white queen

--- src/App.js
+++ src/App.js
@@ -1,64 +1,62 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Components
-import Page404 from './pages/Page404';
-import NavMenu from './layout/NavMenu';
-import Visits from './pages/VisitsNew';
-import GalleryNew from './pages/GalleryNew';
-import Footer from './layout/Footer';
-import Contact from './pages/Contact';
-import Club from './pages/Club';
-import HomeNew from './pages/HomeNew';
-import Wines from './pages/Wines';
-import Cazare from './pages/Cazare';
+import Page404 from "./pages/Page404";
+import NavMenu from "./layout/NavMenu";
+import Visits from "./pages/VisitsNew";
+import GalleryNew from "./pages/GalleryNew";
+import Footer from "./layout/Footer";
+import Contact from "./pages/Contact";
+import Club from "./pages/Club";
+import HomeNew from "./pages/HomeNew";
+import Wines from "./pages/Wines";
+import Cazare from "./pages/Cazare";
 
 // Wines
-import MisterS_Rezerva from './pages/wines/MisterS_Rezerva';
-import CuveeUnchiuPuiu2019 from './pages/wines/CuveeUnchiuPuiu2019';
-import BlauBurg2018 from './pages/wines/BlauBurg2018';
-import whiteQueen_Limited from './pages/wines/WhiteQueen2020_Limited';
-import CuveeFrancisc2019 from './pages/wines/CuveeFrancisc2019';
+import MisterS_Rezerva from "./pages/wines/MisterS_Rezerva";
+import CuveeUnchiuPuiu2019 from "./pages/wines/CuveeUnchiuPuiu2019";
+import BlauBurg2018 from "./pages/wines/BlauBurg2018";
+import CuveeFrancisc2019 from "./pages/wines/CuveeFrancisc2019";
 
 function App() {
-  return (
-    <Router>
-      <NavMenu />
-      <Switch>
-        <Route exact path='/' component={HomeNew} />
-        <Route exact path='/vinuri' component={Wines} />
-        <Route exact path='/vizite-si-degustari' component={Visits} />
-        <Route exact path='/club' component={Club} />
-        <Route exact path='/cazare' component={Cazare} />
-        <Route exact path='/contact' component={Contact} />
-        <Route exact path='/galerie' component={GalleryNew} />
-        <Route
-          exact
-          path='/vinuri/mister-s-rezerva'
-          component={MisterS_Rezerva}
-        />
-        <Route
-          exact
-          path='/vinuri/cuvee-unchiu-puiu-2019'
-          component={CuveeUnchiuPuiu2019}
-        />
-        <Route
-          exact
-          path='/vinuri/cuvee-francisc-2019'
-          component={CuveeFrancisc2019}
-        />
-        <Route exact path='/vinuri/blau-burg-2018' component={BlauBurg2018} />
-        <Route
-          exact
-          path='/vinuri/white-queen-2020'
-          component={whiteQueen_Limited}
-        />
-        <Route component={Page404} />
-      </Switch>
-      <Footer />
-    </Router>
-  );
+    return (
+        <Router>
+            <NavMenu />
+            <Switch>
+                <Route exact path="/" component={HomeNew} />
+                <Route exact path="/vinuri" component={Wines} />
+                <Route exact path="/vizite-si-degustari" component={Visits} />
+                <Route exact path="/club" component={Club} />
+                <Route exact path="/cazare" component={Cazare} />
+                <Route exact path="/contact" component={Contact} />
+                <Route exact path="/galerie" component={GalleryNew} />
+                <Route
+                    exact
+                    path="/vinuri/mister-s-rezerva"
+                    component={MisterS_Rezerva}
+                />
+                <Route
+                    exact
+                    path="/vinuri/cuvee-unchiu-puiu-2019"
+                    component={CuveeUnchiuPuiu2019}
+                />
+                <Route
+                    exact
+                    path="/vinuri/cuvee-francisc-2019"
+                    component={CuveeFrancisc2019}
+                />
+                <Route
+                    exact
+                    path="/vinuri/blau-burg-2018"
+                    component={BlauBurg2018}
+                />
+                <Route component={Page404} />
+            </Switch>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
--- src/pages/Wines.js
+++ src/pages/Wines.js
@@ -1,103 +1,94 @@
-import React, { useEffect, useRef } from 'react';
-import '../css/wines.css';
-import Img from 'react-image';
-import { Container, Row } from 'react-bootstrap';
-import { Helmet } from 'react-helmet';
+import React, { useEffect, useRef } from "react";
+import "../css/wines.css";
+import Img from "react-image";
+import { Container, Row } from "react-bootstrap";
+import { Helmet } from "react-helmet";
 
 // Components
-import WineCard from './pagesComponents/WineCard';
+import WineCard from "./pagesComponents/WineCard";
 
 // Images
-import down from '../img/dwn-light.svg';
+import down from "../img/dwn-light.svg";
 // import calx from '../img/wines/CALX2020.png';
-import cunp from '../img/wines/CUNP2019_medal.jpg';
+import cunp from "../img/wines/CUNP2019_medal.jpg";
 // import cosm from '../img/wines/RC2020.png';
 // import ladyblk from '../img/wines/LBK2019_new.png';
-import mrsr from '../img/wines/MRSR2018.png';
+import mrsr from "../img/wines/MRSR2018.png";
 // import rkr from '../img/wines/RKR2018.png';
-import blau from '../img/wines/BLAUB2018.jpg';
-import whq from '../img/wines/WHQ2020_new.jpg';
-import cfr2019 from '../img/wines/CFR2019_new.jpg';
+import blau from "../img/wines/BLAUB2018.jpg";
+// import whq from "../img/wines/WHQ2020_new.jpg";
+import cfr2019 from "../img/wines/CFR2019_new.jpg";
 
 const Wines = () => {
-  const winesRef = useRef(null);
-  useEffect(() => {
-    if (window.location.hash === '') {
-      window.scroll(0, 0);
-    } else {
-      window.scroll(0, winesRef.current.offsetTop + 75);
-    }
-  }, []);
+    const winesRef = useRef(null);
+    useEffect(() => {
+        if (window.location.hash === "") {
+            window.scroll(0, 0);
+        } else {
+            window.scroll(0, winesRef.current.offsetTop + 75);
+        }
+    }, []);
 
-  return (
-    <div id='wines-page'>
-      <Helmet>
-        <title>Crama Ferdi | Vinuri</title>
-      </Helmet>
-      <div className='wines-intro'>
-        <h1
-          data-aos='fade-up'
-          data-aos-duration='700'
-          data-aos-anchor='#start'
-          data-aos-delay='250'>
-          VINURI
-        </h1>
-        <a href='#wines' className='scroll-down'>
-          <Img src={down} alt='Scroll Down' />
-        </a>
-        <div id='wines' ref={winesRef}></div>
-      </div>
-      <Container className='wines-list'>
-        <div className='wines-header'>
-          <h2>Vinuri cu rădăcini în pasiune, curaj și dăruire.</h2>
-          <p>
-            Pentru a comanda vinuri sau pentru degustări şi vizite la cramă,
-            contactaţi-ne la numerele de telefon sau adresa de e-mail din pagina
-            de contact.
-          </p>
+    return (
+        <div id="wines-page">
+            <Helmet>
+                <title>Crama Ferdi | Vinuri</title>
+            </Helmet>
+            <div className="wines-intro">
+                <h1
+                    data-aos="fade-up"
+                    data-aos-duration="700"
+                    data-aos-anchor="#start"
+                    data-aos-delay="250">
+                    VINURI
+                </h1>
+                <a href="#wines" className="scroll-down">
+                    <Img src={down} alt="Scroll Down" />
+                </a>
+                <div id="wines" ref={winesRef}></div>
+            </div>
+            <Container className="wines-list">
+                <div className="wines-header">
+                    <h2>Vinuri cu rădăcini în pasiune, curaj și dăruire.</h2>
+                    <p>
+                        Pentru a comanda vinuri sau pentru degustări şi vizite
+                        la cramă, contactaţi-ne la numerele de telefon sau
+                        adresa de e-mail din pagina de contact.
+                    </p>
+                </div>
+                <Row>
+                    <WineCard
+                        link="/vinuri/cuvee-francisc-2019"
+                        price="110 RON"
+                        img={cfr2019}
+                        title="Cuvée Francisc Ed. Limitată"
+                        year="2019"
+                    />
+                    <WineCard
+                        link="/vinuri/blau-burg-2018"
+                        price="105 RON"
+                        img={blau}
+                        title="Blau Burg Ed. Limitată"
+                        year="2018"
+                    />
+                    <WineCard
+                        link="/vinuri/cuvee-unchiu-puiu-2019"
+                        price="60 RON"
+                        img={cunp}
+                        title="Cuvée Unchiu Puiu"
+                        year="2019"
+                    />
+                    <WineCard
+                        link="/vinuri/mister-s-rezerva"
+                        price="130 RON"
+                        img={mrsr}
+                        title="Mister S Rezerva"
+                        year="2018"
+                    />
+                </Row>
+            </Container>
         </div>
-        <Row>
-          <WineCard
-            link='/vinuri/cuvee-francisc-2019'
-            price='110 RON'
-            img={cfr2019}
-            title='Cuvée Francisc Ed. Limitată'
-            year='2019'
-          />
-          <WineCard
-            link='/vinuri/white-queen-2020'
-            price='80 RON'
-            img={whq}
-            title='White Queen Ed. Limitată'
-            year='2020'
-          />
-          <WineCard
-            link='/vinuri/blau-burg-2018'
-            price='105 RON'
-            img={blau}
-            title='Blau Burg Ed. Limitată'
-            year='2018'
-          />
-          <WineCard
-            link='/vinuri/cuvee-unchiu-puiu-2019'
-            price='60 RON'
-            img={cunp}
-            title='Cuvée Unchiu Puiu'
-            year='2019'
-          />
-        </Row>
-        <Row>
-          <WineCard
-            link='/vinuri/mister-s-rezerva'
-            price='130 RON'
-            img={mrsr}
-            title='Mister S Rezerva'
-            year='2018'
-          />
-        </Row>
-      </Container>
-    </div>
-  );
+    );
 };
 
 export default Wines;
