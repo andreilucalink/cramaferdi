289f81364aa5ed134fc5399d59f7fc8dcb718d97
Andrei Luca <andreilucalink@gmail.com>
2022-11-01 14:41:34 -0700

    wine card styles

--- src/pages/pagesComponents/WineCard.js
+++ src/pages/pagesComponents/WineCard.js
@@ -5,29 +5,30 @@ import Img from "react-image";
 import WineLoader from "../pagesComponents/WineLoader";
 
 const WineCard = ({ img, link, year, title, price }) => {
-    return (
-        <Col className="wine-card" lg={3} md={6} xs={12}>
-            <Col className="wine-description" md={12}>
-                <Img
-                    src={img}
-                    loader={
-                        <div className="loader-wrapper">
-                            <WineLoader />
-                        </div>
-                    }
-                />
-                <div className="wine-info">
-                    <p className="price">{price}</p>
-                    <p>
-                        {title} {year}
-                    </p>
-                    <Link className="wine-details-btn" to={link}>
-                        Vezi mai mult...
-                    </Link>
-                </div>
-            </Col>
-        </Col>
-    );
+  return (
+    <Col className="wine-card" lg={3} md={6} xs={12}>
+      <Col className="wine-description" md={12}>
+        <Img
+          src={img}
+          loader={
+            <div className="loader-wrapper">
+              <WineLoader />
+            </div>
+          }
+          className="border-top-corners"
+        />
+        <div className="wine-info border-bottom-corners">
+          <p className="price">{price}</p>
+          <p>
+            {title} {year}
+          </p>
+          <Link className="wine-details-btn" to={link}>
+            Vezi mai mult...
+          </Link>
+        </div>
+      </Col>
+    </Col>
+  );
 };
 
 export default WineCard;
