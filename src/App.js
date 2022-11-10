95aaa9fe13c6878d25fe0279726445225225a280
Andrei Luca <andreilucalink@gmail.com>
2022-11-10 05:05:28 -0800

    removed puiu

--- src/App.js
+++ src/App.js
@@ -15,54 +15,44 @@ import Cazare from "./pages/Cazare";
 import About from "./pages/About";
 
 // Wines
-import CuveeUnchiuPuiu2020 from "./pages/wines/CuveeUnchiuPuiu2020";
 import WhiteQueen2021 from "./pages/wines/WhiteQueen2021";
 import RedKing2020 from "./pages/wines/RedKing2020";
 import RedKing2020_Legend from "./pages/wines/RedKing2020_Legend";
 import CuveePitigaia2020 from "./pages/wines/CuveePitigaia2020";
 
 function App() {
-    return (
-        <Router>
-            <NavMenu />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/vinuri" component={Wines} />
-                <Route exact path="/vizite-si-degustari" component={Visits} />
-                <Route exact path="/contact" component={Contact} />
-                <Route exact path="/galerie" component={Gallery} />
-                <Route exact path="/despre-noi" component={About} />
-                <Route exact path="/cazare" component={Cazare} />
-                <Route
-                    exact
-                    path="/vinuri/red-king-2020"
-                    component={RedKing2020}
-                />
-                <Route
-                    exact
-                    path="/vinuri/cuvee-pitigaia-2020"
-                    component={CuveePitigaia2020}
-                />
-                <Route
-                    exact
-                    path="/vinuri/red-king-2020-legend"
-                    component={RedKing2020_Legend}
-                />
-                <Route
-                    exact
-                    path="/vinuri/white-queen-2021"
-                    component={WhiteQueen2021}
-                />
-                <Route
-                    exact
-                    path="/vinuri/cuvee-unchiu-puiu-2020"
-                    component={CuveeUnchiuPuiu2020}
-                />
-                <Route component={Page404} />
-            </Switch>
-            <Footer />
-        </Router>
-    );
+  return (
+    <Router>
+      <NavMenu />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/vinuri" component={Wines} />
+        <Route exact path="/vizite-si-degustari" component={Visits} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/galerie" component={Gallery} />
+        <Route exact path="/despre-noi" component={About} />
+        <Route exact path="/cazare" component={Cazare} />
+        <Route exact path="/vinuri/red-king-2020" component={RedKing2020} />
+        <Route
+          exact
+          path="/vinuri/cuvee-pitigaia-2020"
+          component={CuveePitigaia2020}
+        />
+        <Route
+          exact
+          path="/vinuri/red-king-2020-legend"
+          component={RedKing2020_Legend}
+        />
+        <Route
+          exact
+          path="/vinuri/white-queen-2021"
+          component={WhiteQueen2021}
+        />
+        <Route component={Page404} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/Wines.js
+++ src/pages/Wines.js
@@ -11,7 +11,7 @@ import WineCard from "./pagesComponents/WineCard";
 // import calx2021 from "../img/wines/CALX2021_2.jpg";
 // import mads2021 from "../img/wines/MADS2021.jpg";
 // import lbk2020 from "../img/wines/LBK2020.jpg";
-import cunp2020 from "../img/wines/CUNP2020.jpg";
+// import cunp2020 from "../img/wines/CUNP2020.jpg";
 // import cfr2020 from "../img/wines/CFR2020.jpg";
 import whq2021 from "../img/wines/WHQ2021.jpg";
 import rk2020 from "../img/wines/RK2020.jpg";
@@ -19,81 +19,73 @@ import cpit2020 from "../img/wines/CPIT2020_new.jpg";
 import rkl2020 from "../img/wines/RKL2020_new.jpg";
 
 const Wines = () => {
-    const winesRef = useRef(null);
-    useEffect(() => {
-        if (window.location.hash === "") {
-            window.scroll(0, 0);
-        } else {
-            window.scroll(0, winesRef.current.offsetTop + 75);
-        }
-    }, []);
+  const winesRef = useRef(null);
+  useEffect(() => {
+    if (window.location.hash === "") {
+      window.scroll(0, 0);
+    } else {
+      window.scroll(0, winesRef.current.offsetTop + 75);
+    }
+  }, []);
 
-    return (
-        <div id="wines-page">
-            <Helmet>
-                <title>Crama Ferdi | Vinuri</title>
-            </Helmet>
-            <div className="wines-intro">
-                <h1
-                    data-aos="fade-up"
-                    data-aos-duration="700"
-                    data-aos-anchor="#start"
-                    data-aos-delay="250">
-                    VINURI
-                </h1>
-                <div id="wines" ref={winesRef}></div>
-            </div>
-            <Container className="wines-list">
-                <div className="wines-header">
-                    <h2>Vinuri cu rădăcini în pasiune, curaj și dăruire.</h2>
-                    <p>
-                        Pentru a comanda vinuri sau pentru degustări şi vizite
-                        la cramă, contactaţi-ne la numerele de telefon sau la
-                        adresa de e-mail din pagina de contact.
-                    </p>
-                </div>
-                <Row>
-                    <WineCard
-                        link="/vinuri/red-king-2020-legend"
-                        price="150 RON"
-                        img={rkl2020}
-                        title="Red King Legend"
-                        year="2021"
-                    />
-                    <WineCard
-                        link="/vinuri/cuvee-pitigaia-2020"
-                        price="120 RON"
-                        img={cpit2020}
-                        title="Cuveé Piţigaia"
-                        year="2020"
-                    />
-                    <WineCard
-                        link="/vinuri/white-queen-2021"
-                        price="110 RON"
-                        img={whq2021}
-                        title="White Queen Ed. Lim."
-                        year="2021"
-                    />
-                    <WineCard
-                        link="/vinuri/red-king-2020"
-                        price="110 RON"
-                        img={rk2020}
-                        title="Red King Ed. Lim."
-                        year="2020"
-                    />
-                </Row>
-                <Row>
-                    <WineCard
-                        link="/vinuri/cuvee-unchiu-puiu-2020"
-                        price="60 RON"
-                        img={cunp2020}
-                        title="Cuveé Unchiu Puiu"
-                        year="2020"
-                    />
-                </Row>
-            </Container>
+  return (
+    <div id="wines-page">
+      <Helmet>
+        <title>Crama Ferdi | Vinuri</title>
+      </Helmet>
+      <div className="wines-intro">
+        <h1
+          data-aos="fade-up"
+          data-aos-duration="700"
+          data-aos-anchor="#start"
+          data-aos-delay="250"
+        >
+          VINURI
+        </h1>
+        <div id="wines" ref={winesRef}></div>
+      </div>
+      <Container className="wines-list">
+        <div className="wines-header">
+          <h2>Vinuri cu rădăcini în pasiune, curaj și dăruire.</h2>
+          <p>
+            Pentru a comanda vinuri sau pentru degustări şi vizite la cramă,
+            contactaţi-ne la numerele de telefon sau la adresa de e-mail din
+            pagina de contact.
+          </p>
         </div>
-    );
+        <Row>
+          <WineCard
+            link="/vinuri/red-king-2020-legend"
+            price="150 RON"
+            img={rkl2020}
+            title="Red King Legend"
+            year="2021"
+          />
+          <WineCard
+            link="/vinuri/cuvee-pitigaia-2020"
+            price="120 RON"
+            img={cpit2020}
+            title="Cuveé Piţigaia"
+            year="2020"
+          />
+          <WineCard
+            link="/vinuri/white-queen-2021"
+            price="110 RON"
+            img={whq2021}
+            title="White Queen Ed. Lim."
+            year="2021"
+          />
+          <WineCard
+            link="/vinuri/red-king-2020"
+            price="110 RON"
+            img={rk2020}
+            title="Red King Ed. Lim."
+            year="2020"
+          />
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Wines;
